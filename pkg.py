4a79a53117b86986e7dad17b1cbe4357d5aec3f7
Eli Waltuch <eliwaltuch@gmail.com>
2024-06-18 10:12:06 +0300

    Created main() function and ported command line parsing to there added protection for when the user doesn't pass an arg added help option
    Signed-off-by: Eli Waltuch <eliwaltuch@gmail.com>

--- pkg.py
+++ pkg.py
@@ -2,13 +2,24 @@
 
 import sys
 
-cmd = sys.argv[1]
-match cmd:
-    case "install":
-        print("Usage: install PKG")
-    case "uninstall":
-        print("Usage: uninstall PKG")
-    case "list":
-        print("Usage: list")
-    case _:
+def main():
+    if len(sys.argv) < 2:
         print("Usage: pkg {install PKG|uninstall PKG|list}")
+        return
+    cmd = sys.argv[1]
+    match cmd:
+        case "install":
+            print("Usage: install PKG")
+        case "uninstall":
+            print("Usage: uninstall PKG")
+        case "list":
+            print("Usage: list")
+        case "help":
+            print("Usage: pkg {install PKG|uninstall PKG|list}")
+            return
+        case _:
+            print("Invalid argument:", cmd ,"\nUsage: pkg {install PKG|uninstall PKG|list}")
+            return
+
+if __name__ == '__main__':
+    main()

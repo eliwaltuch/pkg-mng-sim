3d59a943393bf84f4b1fb2195d05dc4913bbc659
Eli Waltuch <eliwaltuch@gmail.com>
2024-06-18 10:01:07 +0300

    added switch statement for handling cmd line args
    Signed-off-by: Eli Waltuch <eliwaltuch@gmail.com>

--- pkg.py
+++ pkg.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
 
-print("Usage: pkg {install PKG|uninstall PKG|list}")
+import sys
+
+cmd = sys.argv[1]
+match cmd:
+    case "install":
+        print("Usage: install PKG")
+    case "uninstall":
+        print("Usage: uninstall PKG")
+    case "list":
+        print("Usage: list")
+    case _:
+        print("Usage: pkg {install PKG|uninstall PKG|list}")
